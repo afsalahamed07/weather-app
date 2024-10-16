f32ad4be9a61c1780235390c03628f2f3a376f5b
agrippa <afsalahamed07@gmail.com>
2024-10-16 16:18:22 +0530

    adding weather api by city name

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
+import style from "./style.css";
 import { weather } from "./model/weather";
-import { apiCall, getWeather } from "./infra/openWeatherApi";
+import { getWeatherByCity, getWeather } from "./infra/openWeatherApi";
 import api from "./infra/openWeatherApiKey.json";
 
 const BODY = document.querySelector("body");
-
-const w1 = weather("Burmingom");
-
-console.log(w1.getTemp());
+const weatherBox = document.querySelector(".weather-box");
+const city = document.querySelector(".city");
+const temp = document.querySelector(".temp");
+const cloud = document.querySelector(".cloud");
+const wind = document.querySelector(".wind");
 
 try {
-  const data = await getWeather(33.44, -94.04, api.key);
+  // lat and long of kalmunai, eastern, sri lanka
+  // const data = await getWeather(7.41, 81.82, api.key);
+  const data = await getWeatherByCity('colombo', api.key);
   console.log(data);
+
+  const w1 = weather(data);
+  console.log("name: ", w1.getCity());
+  console.log("temp: ", w1.getTemp());
+
+  city.innerText = w1.getCity();
+  temp.innerText = "Temp: " + w1.getTemp();
+  cloud.innerText = "Cloud: " + w1.getCloud();
+  wind.innerText = "Wind: " + w1.getWind();
 } catch (err) {
-  console.log("Error Occured");
+  console.log(err);
 }
--- src/infra/openWeatherApi.js
+++ src/infra/openWeatherApi.js
@@ -1,5 +1,9 @@
 function apiCall(lat, lon, apiKey) {
-  return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  // return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+}
+
+function apiCallByCity(city, apiKey) {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 }
 
 // Doubt would be why i have to wrap the funcition with async
@@ -21,4 +25,23 @@ async function getWeather(lat, lon, apiKey) {
   }
 }
 
-export { apiCall, getWeather };
+// Doubt would be why i have to wrap the funcition with async
+async function getWeatherByCity(city, apiKey) {
+  try {
+    // returns a promise json weather response from openweather
+
+    // await saying is like sayig pause the exceuction of this funciton
+    // until we get the value
+    const response = await fetch(apiCallByCity(city, apiKey));
+    const weatherData = await response.json();
+
+    // without async the return would be
+    // return Promise.resolve(weatherData)
+    return weatherData;
+  } catch (err) {
+    console.log("Error happened");
+    throw err;
+  }
+}
+
+export { getWeather, getWeatherByCity };
